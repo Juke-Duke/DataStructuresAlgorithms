31a6b60fd01146a356b04c9949f9869d835d8f8a
JukeDuke <masterofegy@gmail.com>
2022-02-27 00:34:00 -0500

    new

--- Algorithms/General.cs
+++ Algorithms/General.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace DataStructuresAlgorithms
 {
@@ -240,6 +239,28 @@ namespace DataStructuresAlgorithms
             }
 
             return binary.Peek().Data == 0 ? Decimal : Decimal * -1;
+        }
+    
+        public static int SieveOfEratosthenes(int n)
+        {
+            int primes = 0;
+            bool[] prime = new bool[n + 1];
+            for (int i = 0; i <= n; i++)
+                prime[i] = true;
+ 
+            for (int p = 2; p * p <= n; p++)
+                if (prime[p] == true)
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i] == true)
+                    Console.Write(i + " ");
+                ++primes;
+            }
+
+            return primes;
         }
     }
 }
